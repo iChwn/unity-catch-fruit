baebd73d22accb7d777e424c603fa78dd1a6d745
ichwn <ichwanarif26@gmail.com>
2022-10-12 09:35:35 +0700

    Initial Project

---
+++ Assets/Scripts/PipeMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMovement : MonoBehaviour
+{
+
+    private Rigidbody2D myBody;
+    public float speed, xBound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        print(xAxis);
+        
+        myBody.velocity = Vector2.zero;
+        if(xAxis > 0) {
+            myBody.velocity = Vector2.right * speed;
+        } else if(xAxis < 0) {
+            myBody.velocity = Vector2.left * speed;
+        }
+
+        transform.position = new Vector2(
+            Mathf.Clamp(transform.position.x, -xBound, xBound),
+            transform.position.y
+        );
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] birds;
+    public GameObject bomb;
+    public float xBounds, yBound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRandomGameObject());
+    }
+
+    IEnumerator SpawnRandomGameObject() {
+        yield return new WaitForSeconds(Random.Range(1, 2));
+
+        int randomBird = Random.Range(0, birds.Length);
+        if(Random.value <= .6f) {
+            Instantiate(
+                birds[randomBird],
+                new Vector2(Random.Range(-xBounds, xBounds), yBound),
+                Quaternion.identity
+            );
+        } else {
+            Instantiate(
+                bomb,
+                new Vector2(Random.Range(-xBounds, xBounds), yBound),
+                Quaternion.identity
+            );
+        }
+
+        StartCoroutine(SpawnRandomGameObject());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
